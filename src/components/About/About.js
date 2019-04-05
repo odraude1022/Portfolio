b4c61efcda0af21d0a1e8f3ca813572a054c26dc
Eduardo Iglesias <eriglesias@ufl.edu>
2019-04-05 14:15:32 -0400

    finished the about component and made the profile picture the right size and shape

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,7 +4,32 @@ const About = props => {
   let resumeData = props.resumeData
   return (
     <div>
-      <h1>About</h1>
+      <img className="profile-pic"  src="images/me.jpg" alt="me" />
+      <h2>About Me</h2>
+      <p>
+       {
+         resumeData.aboutme
+       }
+      </p>
+
+      <h2>Contact Details</h2>
+      <p className="address">
+				   <span>
+            Eduardo Iglesias
+         </span>
+         <br/>
+         <span>
+            (305)-992-7547
+         </span>
+         <br/>
+         <span>
+            {resumeData.website}
+         </span>
+         <br/>
+				   <span>
+            {resumeData.address}
+        </span>
+			</p>
     </div>
   )
 }
