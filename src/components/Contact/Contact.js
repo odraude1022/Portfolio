8852e039db00e46f50d615a8d6c30030c088899c
Eduardo Iglesias <eriglesias@ufl.edu>
2019-04-05 14:29:34 -0400

    finished contact/footer section

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -4,7 +4,12 @@ const Contact = props => {
   let resumeData = props.resumeData
   return (
     <div>
-      <h1>Contact</h1>
+      <p>Feel free to contact me below</p>
+      <h4>{resumeData.website}</h4>
+      <h4>(305)-992-7547</h4>
+      <h4>
+        <a href={resumeData.linkedinId}>Click Here to contact me via LinkedIn</a>
+      </h4>
     </div>
   )
 }
