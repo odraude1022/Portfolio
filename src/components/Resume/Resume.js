ccaefab6a4a85bb8689c87fe63f3b7b8af63af11
Eduardo Iglesias <eriglesias@ufl.edu>
2019-04-05 14:27:39 -0400

    finished resume section

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -4,7 +4,61 @@ const Resume = props => {
   let resumeData = props.resumeData
   return (
     <div>
-      <h1>Resume</h1>
+      <div className="education">
+        <h1>Education</h1>
+        {
+          resumeData.education && resumeData.education.map( item => {
+            return(
+              <div key={item.UniversityName}>
+              <h3>{item.UniversityName}</h3>
+              <p className="info">
+                {item.specialization}
+                <span>&bull;</span> <em>{item.MonthOfPassing} {item.YearOfPassing}</em></p>
+              <p>
+              {item.Achievements}
+              </p>
+              </div>
+            )
+          })
+        }
+      </div>
+      <div className="work">
+        <h1>Professional Experience</h1>
+        {
+          resumeData.work && resumeData.work.map( item => {
+            return(
+              <div key={item.CompanyName}>
+                <h3>{item.CompanyName}</h3>
+                <p>
+                  {item.specialization}
+                  <span>&bull;</span> <em>{item.MonthOfLeaving} {item.YearOfLeaving}</em></p>
+                <p>
+                  {item.Achievements}
+                </p>
+              </div>
+            )
+          })
+        }
+      </div>
+      <div className="skills">
+        <h1>Skills</h1>
+
+        <div className="bars">
+          <ul className="skills">
+            {
+              resumeData.skills && resumeData.skills.map((item) => {
+                return(
+                  <li>
+                    <span className={`bar-expand ${item.skillname.toLowerCase()}`}>
+                    </span><em>{item.skillname}</em>
+                  </li>
+                )
+              })
+            }
+
+  				</ul>
+        </div>
+      </div>
     </div>
   )
 }
