1d0fc398c3b7cd1e54d271439a1f66915743f508
Eduardo Iglesias <eriglesias@ufl.edu>
2019-04-05 14:58:34 -0400

    fixed indentation on header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,20 +3,21 @@ import React from "react";
 const Header = props => {
   let resumeData = props.resumeData
   return (
-    <div>
+    <div className="header">
       <h1>Hi, I'm {resumeData.name}</h1>
       <h3>I'm a {resumeData.role}.{resumeData.roleDescription}</h3>
       <ul className="social">
-                  {
-                    resumeData.socialLinks && resumeData.socialLinks.map(item =>{
-                      return(
-                              <li key={item.name}>
-                                <a href={item.url} target="_blank"><i className={item.className}></i></a>
-                              </li>
-                            )
-                          }
-                    )
-                  }
+        {
+          resumeData.socialLinks && resumeData.socialLinks.map( item => {
+            return(
+              <li key={item.name}>
+                <a href={item.url} target="_blank">
+                  <i className={item.className}></i>
+                </a>
+              </li>
+            )
+          })
+        }
       </ul>
     </div>
   )
