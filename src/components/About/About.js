35df27641c89a2e4975cb5cafbe87ffa9ce87651
Eduardo Iglesias <eriglesias@ufl.edu>
2019-04-08 15:16:44 -0400

    css-ed the about component

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,33 +4,37 @@ const About = props => {
   let resumeData = props.resumeData
   return (
     <section id="about">
-      <div>
-        <img className="profile-pic"  src="images/me.jpg" alt="me" />
-        <h2>About Me</h2>
-        <p>
-         {
-           resumeData.aboutme
-         }
-        </p>
+      <div className="about">
+        <div className="profile-pic-wrap">
+          <img className="profile-pic"  src="images/me.jpg" alt="me" />
+        </div>
+        <div className="about-content">
+          <h2>About Me</h2>
+          <p>
+           {
+             resumeData.aboutme
+           }
+          </p>
 
-        <h2>Contact Details</h2>
-        <p className="address">
-  				   <span>
-              Eduardo Iglesias
-           </span>
-           <br/>
-           <span>
-              (305)-992-7547
-           </span>
-           <br/>
-           <span>
-              {resumeData.website}
-           </span>
-           <br/>
-  				   <span>
-              {resumeData.address}
-          </span>
-  			</p>
+          <h2>Contact Details</h2>
+          <p className="address">
+    				   <span>
+                Eduardo Iglesias
+             </span>
+             <br/>
+             <span>
+                (305)-992-7547
+             </span>
+             <br/>
+             <span>
+                {resumeData.website}
+             </span>
+             <br/>
+    				   <span>
+                {resumeData.address}
+            </span>
+    			</p>
+        </div>
       </div>
     </section>
   )
