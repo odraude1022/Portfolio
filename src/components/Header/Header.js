d66dbb7ebf3bc3552813dc5988735b71773d2c40
Eduardo Iglesias <eriglesias@ufl.edu>
2019-04-08 13:59:29 -0400

    finished css-ing the header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,23 +4,25 @@ const Header = props => {
   let resumeData = props.resumeData
   return (
     <section id="header">
-      <div className="header">
-        <h1>Hi, I'm {resumeData.name}</h1>
-        <h3>I'm a {resumeData.role}.{resumeData.roleDescription}</h3>
-        <ul className="social">
-          {
-            resumeData.socialLinks && resumeData.socialLinks.map( item => {
-              return(
-                <li key={item.name}>
-                  <a href={item.url} target="_blank" rel="noopener noreferrer">
-                    <i className={item.className}></i>
-                  </a>
-                </li>
-              )
-            })
-          }
-        </ul>
-      </div>
+        <div className="header">
+          <h1 className="headline">Hi, I'm {resumeData.name}</h1>
+          <h3 className="sub-headline">I'm a {resumeData.role}.{resumeData.roleDescription}</h3>
+          <div className="social-wrap">
+            <ul className="social">
+              {
+                resumeData.socialLinks && resumeData.socialLinks.map( item => {
+                  return(
+                    <li key={item.name}>
+                      <a href={item.url} target="_blank" rel="noopener noreferrer">
+                        <i className={item.className}></i>
+                      </a>
+                    </li>
+                  )
+                })
+              }
+            </ul>
+          </div>
+        </div>
     </section>
   )
 }
